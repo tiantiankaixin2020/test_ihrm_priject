a05b57b799bc37338a34cbb1443e5876008833cb
870957539 <870957539@qq.com>
2020-05-21 14:58:24 +0800

    ihrm代码集成-在线操作

--- run_suite.py
+++ run_suite.py
@@ -15,3 +15,5 @@ report_name = app.BASE_PATH +"/" +"report/ihrm.html"
 with open(report_name,"wb") as f:
     runner = HTMLTestRunner_PY3.HTMLTestRunner(f,verbosity=2,title="IHRM接口测试报告",description="人力资源模块")
     runner.run(suite)
+
+print("测试增加一行代码，jenkins会不会自动构建")
